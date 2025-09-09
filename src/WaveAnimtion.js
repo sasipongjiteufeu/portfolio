1a0b9a43251b1976fe68c1de299775b516c8c2b8
sasipong jiteufeu <115165630+sasipongjiteufeu@users.noreply.github.com>
2025-09-10 00:07:20 +0700

    commit transition basic

---
+++ src/WaveAnimtion.js
@@ -0,0 +1,17 @@
+
+import './WaveAnimation.css'
+
+function WaveAnimation(){
+    return(
+        <div className='main_container'>
+            <div className='Thesqurt'>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>   
+    )
+     
+    
+}
+export default WaveAnimation;
